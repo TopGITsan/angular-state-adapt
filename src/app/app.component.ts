2b413ef1d2467482a9f24121f5a3f985614decf6
TopGitSan <c.toparcean@gmail.com>
2025-01-08 18:47:49 +0100

    feat(app.component): :sparkles: provide the header into the app component and the sidenav

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,37 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, signal } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatToolbarModule } from '@angular/material/toolbar';
 import { RouterOutlet } from '@angular/router';
-
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { HeaderComponent } from './header/header.component';
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MatSidenavModule, HeaderComponent],
   template: `
-    <h1 class="text-3xl font-bold underline text-red-500">Hello, {{ title }}</h1>
+    <app-header />
+    <mat-sidenav-container class="grow">
+      <mat-sidenav mode="side" [opened]="openSidenav()"
+        >Sidenav content</mat-sidenav
+      >
+      <mat-sidenav-content>
+        <h1 class="text-3xl font-bold underline text-red-500">
+          Hello, {{ title }}
+        </h1>
+        <p>Main content</p>
+      </mat-sidenav-content>
+    </mat-sidenav-container>
+
     <router-outlet />
   `,
-  styles: ``,
+  styles: `
+    :host {
+      @apply flex flex-col h-full;
+    }
+  `,
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class AppComponent {
-  title = 'angular-state-adapt';
+  readonly title = 'angular-state-adapt';
+  readonly openSidenav = signal<boolean>(true);
 }
