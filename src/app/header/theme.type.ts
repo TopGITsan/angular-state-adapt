c6fa775d5ddd2f27a4ff9b1fa3ec16ff72ea7403
TopGitSan <c.toparcean@gmail.com>
2025-01-13 12:29:12 +0100

    feat(theme): :sparkles: create theme types

--- src/app/header/theme.type.ts
+++ src/app/header/theme.type.ts
@@ -1,14 +1,20 @@
 // export const themes = ['os-default', 'dark', 'light'] as const;
 // export type Theme = (typeof themes)[number];
-export enum Theme {
-  'os-default' = 'deblur',
+export enum ThemeIconMap {
+  '' = 'deblur',
   'dark' = 'dark_mode',
   'light' = 'light_mode',
 }
-export type ThemeKey = keyof typeof Theme;
-export const themeValues = Object.keys(Theme).filter(
-  (k): k is ThemeKey => typeof k === 'string' && k in Theme,
+export type ThemeKey = keyof typeof ThemeIconMap;
+export const themeValues = Object.keys(ThemeIconMap).filter(
+  (k): k is ThemeKey => typeof k === 'string' && k in ThemeIconMap,
 );
-export const themeButtonIcons = Object.values(Theme).filter(
-  (k): k is Theme => typeof k === 'string',
+export const themeButtonIcons = Object.values(ThemeIconMap).filter(
+  (k): k is ThemeIconMap => typeof k === 'string',
 );
+
+export function isThemeKey(key: any): boolean {
+  return (
+    key && typeof key === 'string' && themeValues.includes(key as ThemeKey)
+  );
+}
