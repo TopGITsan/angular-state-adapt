1118d589433b731929dee50c5e84326491dcdf3d
TopGitSan <c.toparcean@gmail.com>
2025-01-14 12:11:54 +0100

    feat(app.component): :sparkles: connect store to the component sidenav

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,19 @@
-import { ChangeDetectionStrategy, Component, signal } from '@angular/core';
+import { toggleSidenavChange$ } from '@actions/sidenav.actions';
+import { AsyncPipe } from '@angular/common';
+import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { RouterOutlet } from '@angular/router';
+import { GlobalStoreService } from '@store/global-store.service';
 import { HeaderComponent } from './header/header.component';
+
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, MatSidenavModule, HeaderComponent],
+  imports: [RouterOutlet, MatSidenavModule, HeaderComponent, AsyncPipe],
   template: `
-    <app-header />
+    @let sidenavOpened = sidenavOpened$ | async;
+    <app-header (openSidenav)="toggleSidenav$.next($event)" />
     <mat-sidenav-container class="grow">
-      <mat-sidenav mode="side" [opened]="openSidenav()"
+      <mat-sidenav mode="side" [opened]="sidenavOpened"
         >Sidenav content</mat-sidenav
       >
       <mat-sidenav-content>
@@ -30,5 +35,7 @@ import { HeaderComponent } from './header/header.component';
 })
 export class AppComponent {
   readonly title = 'angular-state-adapt';
-  readonly openSidenav = signal<boolean>(true);
+  private readonly globalStore = inject(GlobalStoreService);
+  readonly sidenavOpened$ = this.globalStore.store.sidenavOpened$;
+  readonly toggleSidenav$ = toggleSidenavChange$;
 }
