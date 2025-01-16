712a291777a58b304fac7d7db307d89a3d8f98d7
TopGitSan <c.toparcean@gmail.com>
2025-01-16 17:34:48 +0100

    feat(app.component): :sparkles: add the animation to the component metadata and wire the animation to the router-outlet container

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,21 +2,29 @@ import { toggleSidenavChange$ } from '@actions/sidenav.actions';
 import { AsyncPipe } from '@angular/common';
 import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
 import { MatSidenavModule } from '@angular/material/sidenav';
-import { RouterOutlet } from '@angular/router';
+import { ActivatedRoute, RouterLink, RouterOutlet } from '@angular/router';
 import { GlobalStoreService } from '@store/global-store.service';
 import { HeaderComponent } from './header/header.component';
+import { routeTransition } from '@shared/animations/route-transition.animation';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, MatSidenavModule, HeaderComponent, AsyncPipe],
+  imports: [
+    RouterOutlet,
+    MatSidenavModule,
+    HeaderComponent,
+    AsyncPipe,
+    RouterLink,
+  ],
   template: `
     @let sidenavOpened = sidenavOpened$ | async;
     <app-header (openSidenav)="toggleSidenav$.next($event)" />
     <mat-sidenav-container class="grow">
-      <mat-sidenav mode="side" [opened]="sidenavOpened"
-        >Sidenav content</mat-sidenav
-      >
-      <mat-sidenav-content>
+      <mat-sidenav mode="side" [opened]="sidenavOpened">
+        <a [routerLink]="'home'"> home </a>
+        <a [routerLink]="'about'"> about </a>
+      </mat-sidenav>
+      <mat-sidenav-content [@routeTransition]="route.snapshot.data">
         <router-outlet />
       </mat-sidenav-content>
     </mat-sidenav-container>
@@ -27,9 +35,11 @@ import { HeaderComponent } from './header/header.component';
     }
   `,
   changeDetection: ChangeDetectionStrategy.OnPush,
+  animations: [routeTransition],
 })
 export class AppComponent {
   private readonly globalStore = inject(GlobalStoreService);
+  protected readonly route = inject(ActivatedRoute);
   readonly sidenavOpened$ = this.globalStore.store.isSidenavOpendState$;
   readonly toggleSidenav$ = toggleSidenavChange$;
 }
