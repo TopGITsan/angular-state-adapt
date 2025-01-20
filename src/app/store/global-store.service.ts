f4b51ea1676acdf9b24dd7b8ae510856f635f7e7
TopGitSan <c.toparcean@gmail.com>
2025-01-20 11:50:04 +0100

    feat(global.store): :sparkles: create a new adapter for the sidenavMode, connect store sources to layout observer

--- src/app/store/global-store.service.ts
+++ src/app/store/global-store.service.ts
@@ -2,21 +2,38 @@ import {
   closeSidenavChange$,
   toggleSidenavChange$,
 } from '@actions/sidenav.actions';
-import { Injectable } from '@angular/core';
+import { BreakpointObserver } from '@angular/cdk/layout';
+import { inject, Injectable } from '@angular/core';
 import { adapt } from '@state-adapt/angular';
 import { getId } from '@state-adapt/core';
+import { toSource } from '@state-adapt/rxjs';
+import { filter } from 'rxjs';
 import { initialGlobalState } from './global-state.type';
 import { globalAdapter } from './global.adapter';
-
 @Injectable({
   providedIn: 'root',
 })
 export class GlobalStoreService {
+  private readonly breakpointObserver = inject(BreakpointObserver);
+
+  sidenavModeOverChange$ = this.breakpointObserver
+    .observe(['(min-width: 960px)'])
+    .pipe(
+      filter((state) => state.matches),
+      toSource('[Global] sidenavModeOverChange$'),
+    );
   readonly store = adapt(initialGlobalState, {
     adapter: globalAdapter,
     sources: {
       toggleIsSidenavOpend: toggleSidenavChange$,
       setIsSidenavOpendFalse: closeSidenavChange$,
+      setSidenavModeOver: this.breakpointObserver
+        .observe(['(max-width: 959px)'])
+        .pipe(
+          filter((state) => state.matches),
+          toSource('[Global] sidenavModeOverChange$'),
+        ),
+      setSidenavModeSide: this.sidenavModeOverChange$,
     },
     path: 'global_' + getId(),
   });
--- src/app/store/global.adapter.ts
+++ src/app/store/global.adapter.ts
@@ -1,14 +1,19 @@
 import { createAdapter, joinAdapters } from '@state-adapt/core';
 import { booleanAdapter } from '@state-adapt/core/adapters';
-import { GlobalState } from './global-state.type';
+import { GlobalState, SidenavMode } from './global-state.type';
 
 const sidenavAdapter = booleanAdapter;
 const errorAdapter = createAdapter<{} | null>()({
   setNull: () => null,
 });
+const sidenavModeAdapter = createAdapter<SidenavMode>()({
+  setSide: () => 'side',
+  setOver: () => 'over',
+});
 export const globalAdapter = joinAdapters<GlobalState>()({
   isSidenavOpend: sidenavAdapter,
   error: errorAdapter,
+  sidenavMode: sidenavModeAdapter,
 })({
   // selectors
 })({
