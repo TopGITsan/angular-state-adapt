1076dcfd4a79859943122c9a5b28059dd4393bed
TopGitSan <c.toparcean@gmail.com>
2025-01-27 11:44:15 +0100

    feat(lang-menu-button): :sparkles: translate language options

--- src/app/transloco/ui/lang-menu-button/lang-menu-button.component.ts
+++ src/app/transloco/ui/lang-menu-button/lang-menu-button.component.ts
@@ -1,46 +1,67 @@
-import { AsyncPipe } from '@angular/common';
+import { AsyncPipe, TitleCasePipe } from '@angular/common';
 import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatMenuModule } from '@angular/material/menu';
+import { provideTranslocoScope, TranslocoDirective } from '@jsverse/transloco';
 import { GlobalStoreService } from '@store/global-store.service';
+import { inlineLoader } from '@transloco/inline-loader.function';
 import { langList } from '@transloco/lang.constants';
 import { languageChange$ } from '@transloco/language.actions';
 
 @Component({
   selector: 'app-lang-menu-button',
-  imports: [MatMenuModule, MatButtonModule, MatIconModule, AsyncPipe],
+  imports: [
+    MatMenuModule,
+    MatButtonModule,
+    MatIconModule,
+    AsyncPipe,
+    TitleCasePipe,
+    TranslocoDirective,
+  ],
+  providers: [
+    provideTranslocoScope({
+      scope: 'lang',
+      loader: inlineLoader(
+        (lang: string, folder: string) =>
+          import(`../../${folder}/${lang}.json`),
+      ),
+    }),
+  ],
   changeDetection: ChangeDetectionStrategy.OnPush,
   standalone: true,
   template: `
-    <button
-      mat-button
-      [matMenuTriggerFor]="menu"
-      aria-label="Language icon-button with a menu"
-    >
-      <mat-icon>arrow_drop_down</mat-icon>
-      {{ selectedLanguage | async }}
-    </button>
-    <mat-menu #menu="matMenu" class="max-h-64">
-      <!--By default, the menu content will be initialized even when the panel is closed. To defer initialization until the menu is open, the content can be provided as an ng-template with the matMenuContent attribute  -->
-      <ng-template matMenuContent class="p-2">
-        @for (lang of langList; track lang) {
-          <button
-            mat-menu-item
-            attr.aria-label="button for language {{ lang }}"
-            (click)="languageChange$.next(lang)"
-          >
-            {{ lang }}
-          </button>
-        }
-      </ng-template>
-    </mat-menu>
+    <ng-container *transloco="let t; prefix: 'lang'">
+      @let selectedLanguage = (selectedLanguage$ | async) ?? '';
+      <button
+        mat-button
+        [matMenuTriggerFor]="menu"
+        aria-label="Language icon-button with a menu"
+      >
+        <mat-icon>arrow_drop_down</mat-icon>
+        {{ t(selectedLanguage) }}
+      </button>
+      <mat-menu #menu="matMenu" class="max-h-64">
+        <!--By default, the menu content will be initialized even when the panel is closed. To defer initialization until the menu is open, the content can be provided as an ng-template with the matMenuContent attribute  -->
+        <ng-template matMenuContent class="p-2">
+          @for (lang of langList; track lang) {
+            <button
+              mat-menu-item
+              attr.aria-label="button for language {{ t(lang) | titlecase }}"
+              (click)="languageChange$.next(lang)"
+            >
+              {{ t(lang) | titlecase }}
+            </button>
+          }
+        </ng-template>
+      </mat-menu>
+    </ng-container>
   `,
   styles: ``,
 })
 export class LangMenuButtonComponent {
   private readonly globalStore = inject(GlobalStoreService);
   readonly langList = langList;
-  readonly selectedLanguage = this.globalStore.store.languageState$;
+  readonly selectedLanguage$ = this.globalStore.store.languageState$;
   readonly languageChange$ = languageChange$;
 }
