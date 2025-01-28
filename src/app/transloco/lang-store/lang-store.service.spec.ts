8f1109306ceab30201365f69f6bc3066da88fcf1
TopGitSan <c.toparcean@gmail.com>
2025-01-28 10:13:36 +0100

    test(lang-store): :white_check_mark: update testing module to pass tests

--- src/app/transloco/lang-store/lang-store.service.spec.ts
+++ src/app/transloco/lang-store/lang-store.service.spec.ts
@@ -1,12 +1,17 @@
 import { TestBed } from '@angular/core/testing';
 
 import { LangStoreService } from './lang-store.service';
+import { defaultStoreProvider } from '@state-adapt/angular';
+import { getTranslocoTestingModule } from '@transloco/get-transloco-testing-module.function';
 
 describe('LangStoreService', () => {
   let service: LangStoreService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [getTranslocoTestingModule()],
+      providers: [defaultStoreProvider],
+    });
     service = TestBed.inject(LangStoreService);
   });
 
