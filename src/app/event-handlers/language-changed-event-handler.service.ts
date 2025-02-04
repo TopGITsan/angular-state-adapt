367668d844c119984b7bb4855336d9708222f477
TopGitSan <c.toparcean@gmail.com>
2025-02-04 19:56:16 +0200

    refactor(imports): :truck: use relative paths

--- src/app/event-handlers/language-changed-event-handler.service.ts
+++ src/app/event-handlers/language-changed-event-handler.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { ApplicationEventHandler } from 'src/app/event-hub/event-handler/application-event-handler.interface';
-import { LanguageChangedEvent } from 'src/app/events/language-change.event';
+import { LanguageChangedEvent } from '../events/language-change.event';
 
 @Injectable()
 export class LanguageChangedEventHandler implements ApplicationEventHandler {
