74cb3639bc4db2d77546c980e604b31ce6208586
TopGitSan <c.toparcean@gmail.com>
2025-02-18 12:57:09 +0100

    feat(welcome): :sparkles: create a service for the welcome component to hold the value provided by the language handler

--- src/app/event-handlers/provider-application-event-handlers.ts
+++ src/app/event-handlers/provider-application-event-handlers.ts
@@ -1,5 +1,5 @@
+import { LanguageChangedEventHandler } from '@welcome/language-changed-event-handler.service';
 import { provideApplicationEventHandler } from '../event-hub/event-handler/provide-application-event-handler.function';
-import { LanguageChangedEventHandler } from './language-changed-event-handler.service';
 
 export const APPLICATION_EVENT_HANDLERS = [
   provideApplicationEventHandler(LanguageChangedEventHandler),
--- src/app/welcome/language-changed-event-handler.service.ts
+++ src/app/welcome/language-changed-event-handler.service.ts
@@ -1,12 +1,14 @@
-import { Injectable } from '@angular/core';
+import { inject, Injectable } from '@angular/core';
 import { ApplicationEventHandler } from '@event-hub/event-handler/application-event-handler.interface';
 import { LanguageChangedEvent } from '../events/language-changed.event';
+import { WelcomeStoreService } from './welcome-store.service';
 
 @Injectable()
 export class LanguageChangedEventHandler implements ApplicationEventHandler {
   eventClass = LanguageChangedEvent;
-
+  private readonly welcomeStore = inject(WelcomeStoreService);
   handle(event: LanguageChangedEvent) {
     console.log({ event });
+    this.welcomeStore.language.next(event.lang);
   }
 }
