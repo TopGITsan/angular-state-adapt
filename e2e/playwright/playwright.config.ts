12d9253243daad73801203bdd8de9f77c4edb3e8
TopGitSan <c.toparcean@gmail.com>
2025-02-25 13:36:11 +0100

    chore(playwright): :heavy_plus_sign: use ngx builder for running e2e tests using @playwright/test in Angular's ng CLI
    For angular applications, this comes with built-in support for Angular CDK's component harnesses
    
    Refs #33

--- e2e/playwright/playwright.config.ts
+++ e2e/playwright/playwright.config.ts
@@ -1,18 +1,12 @@
-import { defineConfig, devices } from '@playwright/test';
+import type { PlaywrightTestConfig } from '@ngx-playwright/test';
+import { devices } from '@ngx-playwright/test';
 
-/**
- * Read environment variables from file.
- * https://github.com/motdotla/dotenv
- */
-// import dotenv from 'dotenv';
-// import path from 'path';
-// dotenv.config({ path: path.resolve(__dirname, '.env') });
+import { join, dirname } from 'node:path';
+import { fileURLToPath } from 'node:url';
 
-/**
- * See https://playwright.dev/docs/test-configuration.
- */
-export default defineConfig({
-  testDir: './e2e/playwright/',
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+const config: PlaywrightTestConfig = {
   /* Run tests in files in parallel */
   fullyParallel: true,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
@@ -21,21 +15,21 @@ export default defineConfig({
   retries: process.env['CI'] ? 2 : 0,
   /* Opt out of parallel tests on CI. */
   workers: process.env['CI'] ? 1 : undefined,
-  /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: 'html',
-  /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
-    browserName: 'chromium',
+    channel: 'chrome',
     headless: true,
-
-    /* Base URL to use in actions like `await page.goto('/')`. */
-    baseURL: 'http://localhost:4200',
-
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
   },
 
-  /* Configure projects for major browsers */
+  testDir: join(__dirname, 'specs'),
+  testMatch: '**/*.e2e-spec.ts',
+
+  reporter: [
+    [process.env['GITHUB_ACTION'] ? 'github' : 'list'],
+    ['junit', { outputFile: join(__dirname, 'results/junit.xml') }],
+  ],
+
   projects: [
     {
       name: 'chromium',
@@ -72,11 +66,6 @@ export default defineConfig({
     //   use: { ...devices['Desktop Chrome'], channel: 'chrome' },
     // },
   ],
+};
 
-  /* Run your local dev server before starting the tests */
-  // webServer: {
-  //   command: 'npm run start',
-  //   url: 'http://127.0.0.1:3000',
-  //   reuseExistingServer: !process.env.CI,
-  // },
-});
+export default config;
